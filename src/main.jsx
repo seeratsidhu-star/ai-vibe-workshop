dd52efe9dbe014a9924e7f6b3178baf0edb21fa1
seeratsidhu-star <seerat_sidhu@trimble.com>
2026-08-18 18:38:16 +0530

    Restore the dark workshop design and remove Gaussian Splatting styles.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,6 @@
 import { createRoot } from "react-dom/client";
-import { defineCustomElements } from "@trimble-oss/moduswebcomponents/loader";
-import { setAssetPath } from "@trimble-oss/moduswebcomponents/components";
 import App from "./App.jsx";
-import "@trimble-oss/moduswebcomponents/modus-wc-styles.css";
 import "./styles/global.css";
 import "./styles/components.css";
-
-setAssetPath(`${window.location.origin}${import.meta.env.BASE_URL}`);
-defineCustomElements();
 
 createRoot(document.getElementById("root")).render(<App />);
